bcb3728175b6106064a96383463bee1bd348cd5d
rozsagyenelaw <rozsagyenelaw@yahoo.com>
2025-09-02 15:58:54 +0200

    Update generate-pdfs.js

--- netlify/functions/generate-pdfs.js
+++ netlify/functions/generate-pdfs.js
@@ -7,9 +7,37 @@ exports.handler = async (event, context) => {
   }
   
   try {
-    const webhookData = JSON.parse(event.body);
+    const data = JSON.parse(event.body);
     
-    // For now, just return success to test if it works
+    // Transform the data with default values
+    const formData = {
+      attorney: {
+        name: data.attorney_name || "ROZSA GYENE, ESQ.",
+        bar_number: "208356",
+        firm_name: "LAW OFFICES OF ROZSA GYENE",
+        street: "450 N BRAND BLVD SUITE 600",
+        city: "GLENDALE",
+        state: "CA",
+        zip: "91203",
+        phone: "818-291-6217",
+        email: "ROZSAGYENELAW@YAHOO.COM",
+      },
+      decedent: {
+        name: data.decedent_name || "Unknown",
+        death_date: data.death_date || "",
+        death_place: data.death_place || "",
+      },
+      petitioner: {
+        name: data.petitioner_name || "Unknown",
+        relationship: data.petitioner_relationship || "",
+      },
+      estate: {
+        has_will: data.has_will === "yes",
+        personal_property: data.personal_property_value || "0",
+      }
+    };
+    
+    // For now, return the transformed data to verify it works
     return {
       statusCode: 200,
       headers: {
@@ -17,8 +45,8 @@ exports.handler = async (event, context) => {
       },
       body: JSON.stringify({ 
         success: true,
-        message: 'Function works!',
-        received: webhookData
+        message: 'Data received and transformed',
+        data: formData
       }),
     };
     
